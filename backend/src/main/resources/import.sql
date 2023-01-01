e496061280e5e058fbcb05ea6a58e3fd08cb4c4d
JAIR RODRIGUES <->
2023-01-01 19:52:24 -0300

    Database seeding

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_client(name) VALUES ('Alice Dias');
+INSERT INTO tb_client(name) VALUES ('Beatriz Rodrigues');
+INSERT INTO tb_client(name) VALUES ('Carlos Gomes');
+INSERT INTO tb_client(name) VALUES ('Davi Metzger');
+INSERT INTO tb_client(name) VALUES ('Ernesto Tomás');
+INSERT INTO tb_client(name) VALUES ('Fernando Silva');
+INSERT INTO tb_client(name) VALUES ('Gustavo José');
+INSERT INTO tb_client(name) VALUES ('Henrique Busatto');
+INSERT INTO tb_client(name) VALUES ('Israel Alcantara');
+INSERT INTO tb_client(name) VALUES ('Jair Dias');
