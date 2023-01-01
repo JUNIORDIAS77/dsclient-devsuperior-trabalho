16053b7c0255d347803836a602a3bd178bd56b17
JAIR RODRIGUES <->
2023-01-01 20:23:46 -0300

    DTO

--- backend/src/main/java/com/dsclient/demo/services/ClientService.java
+++ backend/src/main/java/com/dsclient/demo/services/ClientService.java
@@ -1,11 +1,13 @@
 package com.dsclient.demo.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.dsclient.demo.dto.ClientDTO;
 import com.dsclient.demo.entities.Client;
 import com.dsclient.demo.repositories.ClientRepository;
 
@@ -16,7 +18,10 @@ public class ClientService {
 	private ClientRepository repository;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll() {
-		return repository.findAll();
+	public List<ClientDTO> findAll() {
+		List<Client> list = repository.findAll();
+		return list.stream().map(x -> new ClientDTO(x)).collect(Collectors.toList());
+		
+		
 	}
 }
