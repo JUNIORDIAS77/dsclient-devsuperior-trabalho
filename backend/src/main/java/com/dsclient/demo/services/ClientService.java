c6b300f1aba63a78b90063b2301429e8b257d829
JAIR RODRIGUES <->
2023-01-06 20:27:19 -0300

    Auditing data

--- backend/src/main/java/com/dsclient/demo/services/ClientService.java
+++ backend/src/main/java/com/dsclient/demo/services/ClientService.java
@@ -7,12 +7,15 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.dsclient.demo.dto.ClientDTO;
 import com.dsclient.demo.entities.Client;
 import com.dsclient.demo.repositories.ClientRepository;
+import com.dsclient.demo.services.exceptions.DatabaseException;
 import com.dsclient.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -55,5 +58,17 @@ public class ClientService {
 			throw new ResourceNotFoundException("Id not found" + id); 
 			}
 		}
+
+	public void delete(Long id) {
+		try {
+		repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found" + id);
+			}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
 }
 
